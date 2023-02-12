6d8d2dac3ff9cb3be9104f3a1a6b4b4737d5fb00
Julian <jlmurraygarcia@gmail.com>
2023-02-12 07:04:29 -0800

    S2L3: Initial setup with 3 components (AppComponent, HeaderComponent, HomesComponent) and their respective CLI created files; 3 of 3 tests passing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {HomesComponent} from "./src/components/homes/homes.component";
+import {HeaderComponent} from "./src/components/header/header.component";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,7 +11,7 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, HomesComponent, HeaderComponent
       ],
     }).compileComponents();
   });
@@ -18,18 +20,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'tdd-playground'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tdd-playground');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tdd-playground app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './src/components/header/header.component';
+import { HomesComponent } from './src/components/homes/homes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomesComponent
+  ],
+  exports: [
+    AppRoutingModule,
+    AppComponent,
+    HeaderComponent,
+    HomesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
