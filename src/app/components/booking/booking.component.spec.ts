1f3254dbfc4ab2b8450baef5f9c0d8c5f2ca28e0
Julian <jlmurraygarcia@gmail.com>
2023-02-13 14:32:48 -0800

    S4L19: Added booking tests & html for price-nightly, check-in-date, and check-out-date params; 20 of 21 tests passing and 1 test pending

--- src/app/components/booking/booking.component.spec.ts
+++ src/app/components/booking/booking.component.spec.ts
@@ -5,10 +5,14 @@ import {MockedHomes} from "../../models/homes.mock";
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 import {HomeModel} from "../../models/home.model";
 
+
 describe('BookingComponent', () => {
   let component: BookingComponent;
   let fixture: ComponentFixture<BookingComponent>;
   let dialogData: { home: HomeModel };
+
+  const elGrabber = (selector: string) => fixture.nativeElement.querySelector(selector);
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -38,17 +42,31 @@ describe('BookingComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  // [x] should show price per night
+  // [x] should show check in date field
+  // [x] should show check out date field
+  // [ ] should show total price
+  // [ ] should show submit button
+  // [ ] should book home after clicking the submit button
+
   it('should show title', () => {
-    const item = fixture.nativeElement.querySelector('[data-test="title"]');
-    expect(item.textContent).toContain('Home 1');
+    const item = elGrabber('[data-test="title"]');
+    expect(item.textContent).toContain(MockedHomes[0].title);
   });
 
-  // should show price per night
-  // should show check in date field
-  // should show check out date field
-  // should show total price
-  // should show submit button
-  // should book home after clicking the submit button
+  it('should show price per night', () => {
+    const item = elGrabber('[data-test="price-nightly"]');
+    expect(item.textContent).toContain(MockedHomes[0].price_nightly);
+  });
 
+  it('should show check in date field', () => {
+    const item = elGrabber('[data-test="check-in-date"]');
+    expect(item).toBeTruthy();
+  });
+
+  it('should show check out date field', () => {
+    const item = elGrabber('[data-test="check-out-date"]');
+    expect(item).toBeTruthy();
+  });
 
 });
--- src/app/models/home.model.ts
+++ src/app/models/home.model.ts
@@ -1,17 +1,20 @@
 export class HomeModel {
-  title: string | undefined;
-  image: string | undefined;
-  location: string | undefined;
+  title: string;
+  image: string;
+  location: string;
+  price_nightly: number;
 
   constructor(json: HomeInterface) {
     this.title = json.title;
     this.image = json.image;
     this.location = json.location;
+    this.price_nightly = json.price_nightly;
   }
 }
 
 export interface HomeInterface {
-  title: string | undefined;
-  image: string | undefined;
-  location: string | undefined;
+  title: string;
+  image: string;
+  location: string;
+  price_nightly: number;
 }
